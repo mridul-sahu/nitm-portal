8a7daf76d0a624d64cf0e73ceae8c213db4b4705
Mridul <mridulsahu01@gmail.com>
2016-07-02 00:00:43 +0530

    Title Change

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def full_title (title = '')
-    base_title = "Ruby on Rails Sample App"
+    base_title = "NITM PORTAL"
     base_title = "#{title} | #{base_title}" unless title.empty?
     base_title
   end
