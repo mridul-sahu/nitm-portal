ab11af37e907cbd374281f6cc5bbfa6b2a3da2e1
Mridul <mridulsahu01@gmail.com>
2016-07-19 19:57:43 +0530

    Made design changes

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,9 @@
 module UsersHelper
 
   # Returns the Gravatar for the given user.
-  def gravatar_for(user, sz = "170" )
+  def gravatar_for(user, sz = "170", ex_class= "" )
     pic_url = "default.jpg"
     pic_url = user.picture.url if user.picture?
-    image_tag(pic_url, alt: user.name, class: "gravatar", size: sz)
+    image_tag(pic_url, alt: user.name, class: "gravatar #{ex_class}", size: sz)
   end
 end
