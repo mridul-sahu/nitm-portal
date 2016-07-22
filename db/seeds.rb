54931e3564266444572c0fb90163100905f4f29b
Mridul <mridulsahu01@gmail.com>
2016-07-22 23:02:38 +0530

    seed file change

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@ User.create!(name:  "Mridul Sahu",
              admin: true,
              activated: true,
              activated_at: Time.zone.now)
+
+if Rails.env.production?
+
+  99.times do |n|
+    name  = Faker::Name.name
+    email = "example-#{n+1}@railstutorial.org"
+    password = "password"
+    User.create!(name:  name,
+                 email: email,
+                 password:              password,
+                 password_confirmation: password,
+                 activated: true,
+                 activated_at: Time.zone.now)
+  end
+
+end
+
+
